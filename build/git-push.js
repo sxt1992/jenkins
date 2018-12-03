549a5ae0bac8a723ae2bb7a387c42c5a1ec16cda
taoxuejiao <taoxuejiao@souche.com>
2018-12-03 19:56:37 +0800

    fix: msg

--- build/git-push.js
+++ build/git-push.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync;
 const testParam = process.argv[2];
 const xuanxiang = process.argv[3];
 
-const commitMessage = `test: testParam=${testParam} & xuanxiang=${xuanxiang}`;
+const commitMessage = `"test: testParam=${testParam} & xuanxiang=${xuanxiang}"`;
 
 execSync(`git add -A && git commit -m ${commitMessage}`, { stdio: 'ignore' });
 
